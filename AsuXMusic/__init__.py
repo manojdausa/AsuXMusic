afbf41cf0a6328893da551435736e9a465ba424f
manojdausa <135802271+manojdausa@users.noreply.github.com>
2023-06-17 13:52:36 +0530

    Update __init__.py

--- AsuXMusic/__init__.py
+++ AsuXMusic/__init__.py
@@ -17,7 +17,7 @@ LOGGER = logging.getLogger(__name__)
 
 
 bot = Client(
-    "AsuXMusic",
+    "Lokesh_dausa",
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
@@ -30,23 +30,23 @@ Abishnoi = Client(
 )
 
 user = PyTgCalls(
-    Abishnoi,
+    dausa_123,
     cache_duration=100,
     overload_quiet_mode=True,
 )
 
-call_py = PyTgCalls(Abishnoi, overload_quiet_mode=True)
+call_py = PyTgCalls(Dausa_123, overload_quiet_mode=True)
 
-OWNER_NAME = "Abishnoi1M"
+OWNER_NAME = "lokesh_dausa"
 F_OWNER = OWNER_ID[0]
 
-with Client("AsuXMusic", API_ID, API_HASH, bot_token=BOT_TOKEN) as app:
+with Client("Lokesh_dausa", API_ID, API_HASH, bot_token=BOT_TOKEN) as app:
     x = app.get_me()
     BOT_ID = x.id
     BOT_NAME = x.first_name + (x.last_name or "")
     BOT_USERNAME = x.username
     BOT_MENTION = x.mention
     BOT_DC_ID = x.dc_id
-with Abishnoi as ass:
+with Dausa_123 as ass:
     getass = ass.get_me()
     ASSISTANT_USERNAME = getass.username
